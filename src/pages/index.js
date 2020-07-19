b76deefa127ad844c1b7a04fc042e12ef1b96316
Abhishek Rai <>
2020-07-19 17:54:13 -0400

    Updating main image

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class IndexPage extends React.Component {
           title="Abhishek Rai's personal blog"
           keywords={[`blog`, `abhi2rai`, `developer`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img style={{ margin: 0 }} src="./placholder.png" alt=" work & play" />
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
