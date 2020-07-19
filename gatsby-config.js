8166e99cb2d4003cc589169dfef400ceddce9694
Abhishek Rai <>
2020-07-18 20:52:48 -0400

    Updating descriptors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `abhi2rai`,
+    author: `Abhishek rai`,
+    description: `A personal blog to help me define`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `abhi2rai`,
     },
   },
   plugins: [
